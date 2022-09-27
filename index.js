a1ded5786220e298f6e4f9db1c239067b19c6ff7
Roy <loganroyjr4@gmail.com>
2022-09-27 09:50:51 -0500

    added functions for engineer and intern

--- index.js
+++ index.js
@@ -4,50 +4,114 @@ const inquirer = require('inquirer')
 inquirer
     .prompt([
         {
-           type: 'input',
-           message: 'What is the managers name?',
-           name: 'managersName', 
+            type: 'input',
+            message: 'What is the managers name?',
+            name: 'managersName',
         },
         {
             type: 'input',
-           message: 'What is you employee ID?',
-           name: 'employeeID', 
+            message: 'What is your employee ID?',
+            name: 'employeeID',
         },
         {
             type: 'input',
-           message: 'What is your email address?',
-           name: 'emailAddress', 
+            message: 'What is your email address?',
+            name: 'emailAddress',
         },
         {
             type: 'input',
-           message: 'What is your office number?',
-           name: 'officeNumber', 
+            message: 'What is your office number?',
+            name: 'officeNumber',
         }
     ])
-    .then((answers)=>{
+    .then((answers) => {
         // creation of manager class
         buildTeam()
     })
 
-    function buildTeam(){
-        inquirer
-            .prompt([
-                {
-                    type: 'list',
-                    message: 'Do you want to add an engineer an intern or to finish building your team?',
-                    name: 'buildChoice',
-                    choices: ['engineer','intern','finish building team']
-                }
-            ])
-            .then((answers)=> {
-                switch(answers.buildChoice){
-                    case "engineer":
-                        addEngineer();
-                    case "intern":
-                        addIntern();
-                    case "finish building team"  
-                        finishBuildingTeam();  
-                }
-            })
+function buildTeam() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Do you want to add an engineer an intern or to finish building your team?',
+                name: 'buildChoice',
+                choices: ['engineer', 'intern', 'finish building team']
+            }
+        ])
+        .then((answers) => {
+            switch (answers.buildChoice) {
+                case "engineer":
+                    addEngineer();
+                case "intern":
+                    addIntern();
+                case "finish building team":
+                    finishBuildingTeam();
+            }
+        })
 
-    }
+}
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the engineers name?',
+                name: 'engineersName',
+            },
+            {
+                type: 'input',
+                message: 'What is your employee ID?',
+                name: 'employeeID',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'emailAddress',
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'userName',
+            }
+        ])
+        .then((answers) => {
+            buildTeam()
+            
+        })
+}
+
+function addIntern(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the interns name?',
+                name: 'internsName',
+            },
+            {
+                type: 'input',
+                message: 'What is your employee ID?',
+                name: 'employeeID',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'emailAddress',
+            },
+            {
+                type: 'input',
+                message: 'What school did you attend?',
+                name: 'schoolName',
+            }
+        ])
+        .then((answers) => {
+            buildTeam()
+            
+        })
+}
+
+function finishBuildingTeam(){
+    fs.writeFileSync('index.html',generateHTML(answers))
+}
